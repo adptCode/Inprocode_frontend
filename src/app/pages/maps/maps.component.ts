00dc29167914c7f1f5a6cc80fcfb3ba6563db015
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-05-27 22:09:39 +0200

    maps-update

--- src/app/pages/maps/maps.component.ts
+++ src/app/pages/maps/maps.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 import { environment } from '../../../environments/environment';
 import { Stadium } from '../../interfaces/stadium';
 import { StadiumService } from '../../services/stadium.service';
+import { Marker } from '../../interfaces/markers';
+import { MarkersService } from '../../services/markers.service';
 
 @Component({
   selector: 'app-maps',
@@ -11,20 +13,25 @@ import { StadiumService } from '../../services/stadium.service';
   templateUrl: './maps.component.html',
   styleUrl: './maps.component.css'
 })
-export class MapsComponent implements OnInit{
+export class MapsComponent implements OnInit, OnDestroy{
 
   public map!: mapboxgl.Map;
   stadiums: Stadium[] = [];
+  markers: Marker[] = [];
+  mapMarkers: mapboxgl.Marker[] = [];
+  markerName: string = '';
 
-  constructor(private _stadiumService: StadiumService) {}
-
+  constructor(private _stadiumService: StadiumService, private _markerService: MarkersService) {}
 
   ngOnInit() {
 
     this.initializeMap();
-    this.loadStadiums();
+    //this.loadStadiums();
+    this.loadMarkers();
+  }
 
-
+  ngOnDestroy(): void {
+    this.clearMarkers();
   }
 
   initializeMap(): void {
@@ -32,11 +39,20 @@ export class MapsComponent implements OnInit{
     this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [9.1185503, 45.4784638],
-      zoom: 4 
+      center: [2.1745004, 41.3907372],
+      zoom: 12
+    });
+
+    this.map.on('click', (event) => {
+      const coordinates = event.lngLat;
+      const name = prompt("Enter the name for the marker:");
+      if (name) {
+        this.addMarker({ latitude: coordinates.lat, longitude: coordinates.lng, name });
+      }
     });
   }
 
+  /*
   loadStadiums(): void {
     this._stadiumService.getStadiums().subscribe(stadiums => {
       this.stadiums = stadiums;
@@ -55,4 +71,41 @@ export class MapsComponent implements OnInit{
     });
   }
 
+  */
+
+loadMarkers(): void {
+  this._markerService.getMarkers().subscribe(markers => {
+    this.markers = markers;
+    this.addExistingMarkers();
+  });
 }
+
+addExistingMarkers(): void {
+  this.markers.forEach(marker => {
+    new mapboxgl.Marker({ color: 'blue' })
+      .setLngLat([marker.longitude, marker.latitude])
+      .setPopup(new mapboxgl.Popup({ offset: 25 }).setText(marker.name))
+      .addTo(this.map);
+  });
+}
+
+addMarker(marker: Partial<Marker>): void {
+  this._markerService.createMarker(marker).subscribe(newMarker => {
+    this.markers.push(newMarker);
+    new mapboxgl.Marker({ color: 'blue' })
+      .setLngLat([newMarker.longitude, newMarker.latitude])
+      .setPopup(new mapboxgl.Popup({ offset: 25 }).setText(newMarker.name))
+      .addTo(this.map);
+  });
+}
+
+clearMarkers(): void {
+  this.mapMarkers.forEach(marker => marker.remove());
+    this.mapMarkers = [];
+    this._markerService.deleteAllMarkers().subscribe(() => {
+      this.markers = [];
+    });
+  }
+}
+
+
---
+++ src/app/services/markers.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Marker } from '../interfaces/markers';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarkersService {
+
+  private apiUrl = 'http://localhost:3000/api/markers';
+
+
+  constructor(private http: HttpClient) { }
+
+  getMarkers(): Observable<Marker[]> {
+    return this.http.get<Marker[]>(this.apiUrl);
+  }
+
+  createMarker(marker: Partial<Marker>): Observable<Marker> {
+    return this.http.post<Marker>(this.apiUrl, marker);
+  }
+
+  deleteAllMarkers(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/all`);
+  }
+}
